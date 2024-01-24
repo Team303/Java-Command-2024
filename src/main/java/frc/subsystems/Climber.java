cb509abe5c337a8a07eb6874ffd47a97e8bfefd6
root <root@Team303Surface>
2024-01-23 21:49:20 -0500

    climber basically done i think

---
+++ src/main/java/frc/subsystems/Climber.java
@@ -0,0 +1,64 @@
+package frc.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climber extends SubsystemBase {
+    
+    private final TalonFX armOne;
+    private final TalonFX armTwo;
+
+    private final DigitalInput armOneLimitSwitch;
+    private final DigitalInput armTwoLimitSwitch;
+
+    public Climber() {
+        armOne = new TalonFX(0);
+        armTwo = new TalonFX(1);
+
+        armOneLimitSwitch = new DigitalInput(0);
+        armTwoLimitSwitch = new DigitalInput(1);
+    }
+
+    public void setArmOneSpeed(double speed) {
+        if (!isArmOneAtLimit()) {
+            armTwo.set(speed);
+        } else {
+            armTwo.set(0);
+        }
+    }
+
+    public void setArmTwoSpeed(double speed) {
+        if (!isArmTwoAtLimit()) {
+            armTwo.set(speed);
+        } else {
+            armTwo.set(0);
+        }
+    }
+
+    public void stopArms() {
+        armOne.set(0);
+        armTwo.set(0);
+    }
+
+    public void extendArms() {
+        double speed = 1.0;
+        setArmOneSpeed(speed);
+        setArmTwoSpeed(speed);
+    }
+
+    public boolean isArmOneAtLimit() {
+        return armOneLimitSwitch.get();
+    }
+
+    public boolean isArmTwoAtLimit() {
+        return armTwoLimitSwitch.get();
+    }
+
+    @Override
+    public void periodic() {
+        // Called once per scheduler run
+    }
+}
