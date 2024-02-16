013ad96c9416f4266dc2cd770a33b34bed3b78e0
astro-develops <astromp777@gmail.com>
2024-02-15 19:44:59 -0500

    Arav - "Put in folder to avoid issues when we merge branches. It's auto fix the import paths, just commit"

---
+++ src/main/java/frc/commands/climb/Climb.java
@@ -0,0 +1,31 @@
+package frc.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Robot;
+
+public class Climb extends Command {
+
+    public Climb() {
+        addRequirements(Robot.climber);
+    }
+
+    @Override
+    public void initialize() {
+        // any setup code can go here
+    }
+
+    @Override
+    public void execute() {
+        Robot.climber.extendArms(0.5);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return Robot.climber.isArmAtLimit(1) && Robot.climber.isArmAtLimit(2);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        Robot.climber.stopArms();
+    }
+}
