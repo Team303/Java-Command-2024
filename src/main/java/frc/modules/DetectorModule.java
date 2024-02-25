572c6313a03204cc64edb5a99520bfe8bb903c99
Baritra1 <aritraandbanerjee@gmail.com>
2024-02-24 17:17:58 -0800

    Bump to 3.1 and add raw data

--- src/main/java/frc/modules/DetectorModule.java
+++ src/main/java/frc/modules/DetectorModule.java
@@ -1,9 +1,13 @@
 
 package frc.modules;
 
+import static frc.robot.RobotMap.PhotonvisionConstants.CAMERA_MATRIX;
+import static frc.robot.RobotMap.PhotonvisionConstants.DIST_COEFFS;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -19,14 +23,17 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.util.LowestAmbiguityComparator;
 import frc.robot.util.Point2d;
 import frc.robot.util.TrackedNote;
 
-import static frc.robot.RobotMap.PhotonvisionConstants.CAMERA_MATRIX;
-import static frc.robot.RobotMap.PhotonvisionConstants.DIST_COEFFS;
-
 public class DetectorModule extends SubsystemBase {
+
+    public class LowestAmbiguityComparator implements Comparator<PNPResult> {
+        @Override
+        public int compare(PNPResult one, PNPResult two) {
+            return Double.compare(one.ambiguity, two.ambiguity);
+        }
+    }
 
     public static final ShuffleboardTab DETECTOR_TAB = Shuffleboard.getTab("Detector");
 
@@ -40,15 +47,14 @@ public class DetectorModule extends SubsystemBase {
     public List<TrackedNote> getLatestResult2D() {
         return entries2D;
     }
-    
+
     public List<PNPResult> getLatestResult3D() {
         return entries3D;
     }
 
     public boolean hasTargets() {
-        return entries2D.size()>0;
+        return entries2D.size() > 0;
     }
-
 
     public PNPResult getBestTarget() {
         if (hasTargets()) {
@@ -56,7 +62,6 @@ public class DetectorModule extends SubsystemBase {
         }
         return null;
     }
-
 
     public Transform3d getPosition() {
         return getBestTarget().best;
@@ -66,28 +71,30 @@ public class DetectorModule extends SubsystemBase {
      * Assuming that only notes are being detected
      * TODO: Calibrate PiCam, also no idea if this works
      */
-    public List<PNPResult> solvePNP(List<TrackedNote> boundingBoxes){
+    public List<PNPResult> solvePNP(List<TrackedNote> boundingBoxes) {
         List<PNPResult> results = new ArrayList<>();
-        for(TrackedNote box : boundingBoxes){
-            List<Point2d> points=box.getRectCorners();
-            Point2d tl=points.get(1);
-            Point2d br=points.get(0);
-            Point[] imagePoints = {new Point(tl.x,tl.y),new Point(br.x,tl.y),new Point(br.x,br.y),new Point(tl.x,br.y)};
-            List<Translation3d>  modelTrls = Arrays.asList(new Translation3d(0,-0.1778,0.1778),new Translation3d(0,0.1778,0.1778),new Translation3d(0,0.1778,-0.1778),new Translation3d(0,-0.1778,-0.1778));
+        for (TrackedNote box : boundingBoxes) {
+            List<Point2d> points = box.getRectCorners();
+            Point2d tl = points.get(1);
+            Point2d br = points.get(0);
+            Point[] imagePoints = { new Point(tl.x, tl.y), new Point(br.x, tl.y), new Point(br.x, br.y),
+                    new Point(tl.x, br.y) };
+            List<Translation3d> modelTrls = Arrays.asList(new Translation3d(0, -0.1778, 0.1778),
+                    new Translation3d(0, 0.1778, 0.1778), new Translation3d(0, 0.1778, -0.1778),
+                    new Translation3d(0, -0.1778, -0.1778));
             results.add(OpenCVHelp.solvePNP_SQPNP(CAMERA_MATRIX, DIST_COEFFS, modelTrls, imagePoints));
         }
-        Collections.sort(results,new LowestAmbiguityComparator());
+        Collections.sort(results, new LowestAmbiguityComparator());
         return results;
     }
-
 
     @Override
     public void periodic() {
         entries2D.clear();
         String[] rawEntries = ENTRIES.getStringArray(null);
-        if(rawEntries!=null){
-            for(String entry : rawEntries) {
-                //no idea if this is correct
+        if (rawEntries != null) {
+            for (String entry : rawEntries) {
+                // no idea if this is correct
                 Pattern pattern = Pattern.compile(" |,");
                 double[] formatted = (pattern.splitAsStream(entry).mapToDouble(Double::parseDouble).toArray());
                 entries2D.add(new TrackedNote(formatted));
--- src/main/java/frc/robot/util/LowestAmbiguityComparator.java
+++ src/main/java/frc/robot/util/LowestAmbiguityComparator.java
@@ -4,9 +4,3 @@ import java.util.Comparator;
 
 import org.photonvision.targeting.PNPResult;
 
-public class LowestAmbiguityComparator implements Comparator<PNPResult> {
-    @Override
-    public int compare(PNPResult one, PNPResult two){
-        return Double.compare(one.ambiguity,two.ambiguity);
-    }
-}
